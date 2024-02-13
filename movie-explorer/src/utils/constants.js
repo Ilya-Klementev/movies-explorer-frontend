eb552903d5c30b722d4d955d4f914c61a7613d65
Ilya-Klementev <121625970+Ilya-Klementev@users.noreply.github.com>
2024-02-13 06:44:24 +0100

    Update constants.js

--- movie-explorer/src/utils/constants.js
+++ movie-explorer/src/utils/constants.js
@@ -1,5 +1,5 @@
 const adressMoviesServer = 'https://api.nomoreparties.co';
-const adressMainServer = 'http://localhost:3000';
+const adressMainServer = 'https://api.movieshub.nomoredomainsmonster.ru';
 
 const SCREEN_SMALL = 320;
 const SCREEN_MEDIUM = 768;
